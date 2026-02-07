610577911b295c68282e57b9495cd19cf301be9d
Habib Ahmed <habibahmed1928@gmail.com>
2026-02-07 22:53:26 +0500

    Decimal Places
    Removed comments about default and specific decimal precision outputs.

--- task9.c
+++ task9.c
@@ -6,13 +6,10 @@ int main() {
     printf("Enter a decimal number: ");
     scanf("%lf", &value);
 
-    // Default precision (usually 6 decimal places)
     printf("Default Precision: %lf\n", value);
 
-    // %.2lf limits output to 2 decimal places
     printf("2 Decimal Places:  %.2lf\n", value);
 
-    // %.5lf limits output to 5 decimal places
     printf("5 Decimal Places:  %.5lf\n", value);
 
     return 0;
